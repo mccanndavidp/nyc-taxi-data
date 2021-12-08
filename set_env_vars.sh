98226db56aa63699e3c499305706b293400c1d45
mccanndavidp <mccanndavidp@gmail.com>
2021-12-08 14:46:54 +0000

    Update set_env_vars.sh

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -3,6 +3,6 @@ echo ----
 export BUCKETNAME=alnopd-041800001m1p-eddp-data-processing
 export PREFIX=d3dae
 export INPUT_SUB_PATH=input/dmce6/taxidata
-export INPUT_SUB_PATH=output/dmce6/taxidata
+export OUTPUT_SUB_PATH=output/dmce6/taxidata
 export SRC_SUB_PATH=src/dmce6
 export NUMFILES=5
