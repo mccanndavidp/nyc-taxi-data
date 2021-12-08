faf835fe86bf0df2ae9c05e9dcab90e5f21cc22c
mccanndavidp <mccanndavidp@gmail.com>
2021-12-08 11:05:42 +0000

    Create set_env_vars.sh
    set aws BUCKETNAME, PREFIX , SUBPATHS

---
+++ set_env_vars.sh
@@ -0,0 +1,7 @@
+env
+echo ----
+export BUCKETNAME=alnopd-041800001m1p-eddp-data-processing
+export PREFIX=d3dae
+export INPUT_SUB_PATH=input/dmce6/taxidata
+export INPUT_SUB_PATH=output/dmce6/taxidata
+export SRC_SUB_PATH=src/dmce6
